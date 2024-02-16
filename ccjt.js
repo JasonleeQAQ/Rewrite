05acedeed80fccbcd89fb1225fa8d440f8851a24
JasonleeQAQ <60554818+JasonleeQAQ@users.noreply.github.com>
2024-02-16 22:10:41 +0800

    Update ccjt.js

--- ccjt.js
+++ ccjt.js
@@ -11,11 +11,11 @@ let obj = JSON.parse($response.body);
 
 if ($request.url.includes("http://cc.lzjoy.com/?urlparam=common/user/GetVip") || $request.url.includes("http://cc.lzjoy.com/?urlparam=pad/opern/detail")) {
     obj.list.is_buy = "1";
-    obj.list.is_vip = "3";
+    obj.list.is_vip = "1";
     obj.list.has_buy = "1";
     obj.list.expire_date = "2099-09-09";
     obj.list.vip_expire_date = "2099-09-09";
-    obj.list.vip_type = "3";
+    obj.list.vip_type = "1023";
     obj.list.vip_will_expire_year = "99";
     obj.list.svip_expire_date = "2099-09-09";
     obj.list.vip_will_expire = "2099-09-09";
@@ -24,11 +24,11 @@ if ($request.url.includes("http://cc.lzjoy.com/?urlparam=common/user/GetVip") ||
     obj.list.vip_num = "3";
 } else if ($request.url.includes("http://cc.lzjoy.com/?urlparam=pad/course/getCourseDetailForSingleOpern")) {
     obj.list.is_buy = "1";
-    obj.list.is_vip = "3";
+    obj.list.is_vip = "1";
     obj.list.has_buy = "1";
     obj.list.expire_date = "2099-09-09";
     obj.list.vip_expire_date = "2099-09-09";
-    obj.list.vip_type = "3";
+    obj.list.vip_type = "1023";
     obj.list.vip_will_expire_year = "99";
     obj.list.svip_expire_date = "2099-09-09";
     obj.list.vip_will_expire = "2099-09-09";
